65cc2c4a1e2639df08a10e2ff7b7d574af2480a4
ZhyvelM <zhyvelm@gmail.com>
2025-05-20 12:08:01 +0300

    fixed line height in text styles

---
+++ lib/ui_kit/app_text_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  TextStyle largeTitle = TextStyle(fontWeight: FontWeight.w500, fontSize: 32, height: 19 / 16, letterSpacing: 0);
+  TextStyle title = TextStyle(fontWeight: FontWeight.w500, fontSize: 20, height: 1.6, letterSpacing: 0.5);
+  TextStyle button = TextStyle(fontWeight: FontWeight.w500, fontSize: 14, height: 12 / 7, letterSpacing: 0.16);
+  TextStyle body = TextStyle(fontWeight: FontWeight.w500, fontSize: 16, height: 1.25, letterSpacing: 0);
+  TextStyle subhead = TextStyle(fontWeight: FontWeight.w500, fontSize: 14, height: 10 / 7, letterSpacing: 0);
+}
--- lib/ui_kit/text_styles.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-class AppTextStyle {
-  TextStyle largeTitle = TextStyle(fontWeight: FontWeight.w500, fontSize: 32, height: 38, letterSpacing: 0);
-  TextStyle title = TextStyle(fontWeight: FontWeight.w500, fontSize: 20, height: 32, letterSpacing: 0.5);
-  TextStyle button = TextStyle(fontWeight: FontWeight.w500, fontSize: 14, height: 24, letterSpacing: 0.16);
-  TextStyle body = TextStyle(fontWeight: FontWeight.w500, fontSize: 16, height: 20, letterSpacing: 0);
-  TextStyle subhead = TextStyle(fontWeight: FontWeight.w500, fontSize: 14, height: 20, letterSpacing: 0);
-}
