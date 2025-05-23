fa53d5d9484a5896601eee38cc2d380e4d58ee40
ZhyvelM <zhyvelm@gmail.com>
2025-05-23 15:28:51 +0300

    added localisation support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:todolist/features/add_edit_screen_feature/presentation/add_edit_screen.dart';
+import 'package:todolist/ui_kit/palette.dart';
 
 void main() {
+  initializeDateFormatting();
   runApp(const MyApp());
 }
 
@@ -9,6 +14,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'To Do List');
+    final palette = Palette.of(context);
+    return MaterialApp(
+      title: 'To Do List',
+      localizationsDelegates: [GlobalMaterialLocalizations.delegate],
+      supportedLocales: [const Locale('en', ''), const Locale('ru', '')],
+    );
   }
 }
