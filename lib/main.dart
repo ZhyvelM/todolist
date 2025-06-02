8fccd9aab5293b1c3cfebe72617081e34a577a66
ZhyvelM <zhyvelm@gmail.com>
2025-06-02 10:11:12 +0300

    added bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:todolist/features/main_screen_feature/presentation/main_screen.dart';
+import 'package:todolist/utils/simple_bloc_observer.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
   initializeDateFormatting();
   runApp(const MyApp());
 }
---
+++ lib/utils/simple_bloc_observer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    if (kDebugMode) {
+      print(event);
+    }
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onChange(BlocBase cubit, Change change) {
+    if (kDebugMode) {
+      print(change);
+    }
+    super.onChange(cubit, change);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    if (kDebugMode) {
+      print(transition);
+    }
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    if (kDebugMode) {
+      print(error);
+    }
+    super.onError(bloc, error, stackTrace);
+  }
+}
